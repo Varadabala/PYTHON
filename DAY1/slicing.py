356ce3c15f832edfabc95d089845172942db766c
Varadabala <varadabala446@gmail.com>
2025-10-14 13:15:11 +0530

    slicing

---
+++ DAY1/slicing.py
@@ -0,0 +1,15 @@
+#print only word using slicing
+
+a = "PYTHON PROGRAMMING"
+print(a[0:6])
+print(a[7:18])
+print(a[2:6])
+print(a[2:11])
+print(a[::2])
+
+print(a[::-1])
+print(a[-1:-11:-1])
+print(a[-13:-19:-1])
+print(a[-4:-12:-1])
+print(a[-1:: -2])
+
