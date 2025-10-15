4b341b534d8bfce5a256639f6d787598437e9f86
Varada Bala Narasimhulu <135306416+Varadabala@users.noreply.github.com>
2025-10-15 15:18:37 +0530

    Update common_elements.py

--- Day-2/common_elements.py
+++ Day-2/common_elements.py
@@ -10,8 +10,23 @@ for i in list1:
 
 print(list1)
 print(list2)
-print(common)        
+print(common)  
+__________________________________________________________________
 
+list1 = [1,2,3 ,4,5,6]
+list2 = [1,2,3, 6,7,8 ,9]
+
+common = []
+
+for i in list1:
+    for j in list2:
+        if i == j:
+            common.append(i)
+
+print(list1)
+print(list2)
+print(common)
+______________________________________________________________________
 """
 output 
 
@@ -20,4 +35,5 @@ Enter the elements of list2 : 4 5 6 7 8
 ['1', '2', '3', '4', '5']
 ['4', '5', '6', '7', '8']
 ['4', '5']
+
 """
