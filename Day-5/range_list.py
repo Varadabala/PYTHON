16db507c65cf4a75e438f865fb608b4630bba1b4
Varada Bala Narasimhulu <135306416+Varadabala@users.noreply.github.com>
2025-10-16 12:48:18 +0530

    Update range_list.py with output example
    Added output example for index and value printing.

--- Day-5/range_list.py
+++ Day-5/range_list.py
@@ -7,3 +7,14 @@ for  i in range(0,len(list1)):
     print(f"index : {i} , value : {list1[i]}")
     #dict[i]=list1[i]
 #print(dict)
+
+"""
+output 
+
+Enter a list values : Bala Varada Dhoni
+['Bala', 'Varada', 'Dhoni']
+index : 0 , value : Bala
+index : 1 , value : Varada
+index : 2 , value : Dhoni
+
+"""
