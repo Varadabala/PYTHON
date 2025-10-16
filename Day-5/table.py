a6b212434d393fc40ff6f27464c9ae9298378f42
Varada Bala Narasimhulu <135306416+Varadabala@users.noreply.github.com>
2025-10-16 12:49:41 +0530

    Include output example in multiplication table script
    Added output example for multiplication table.

--- Day-5/table.py
+++ Day-5/table.py
@@ -4,4 +4,23 @@ n = int(input("Enter the number of a table : "))
 a = int(input("Enter the end number : "))
 
 for i in range(1,a+1):
+
     print(f"{n} x {i} = ",n*i)
+
+
+""""
+output 
+Enter the number of a table : 5
+Enter the end number : 10
+5 x 1 =  5
+5 x 2 =  10
+5 x 3 =  15
+5 x 4 =  20
+5 x 5 =  25
+5 x 6 =  30
+5 x 7 =  35
+5 x 8 =  40
+5 x 9 =  45
+5 x 10 =  50
+
+"""
