3e8a80e5ecae01ddae1c65f0d2b0c35db7bbd1dc
Varada Bala Narasimhulu <135306416+Varadabala@users.noreply.github.com>
2025-10-17 11:25:37 +0530

    Refactor multiplication table to include nested loop

--- Day-5/table.py
+++ Day-5/table.py
@@ -3,24 +3,34 @@
 n = int(input("Enter the number of a table : "))
 a = int(input("Enter the end number : "))
 
-for i in range(1,a+1):
-
-    print(f"{n} x {i} = ",n*i)
-
+for j in range(1,n+1):
+    for i in range(1,a+1):
+        print(f"{j} x {i} = ",j*i)
 
 """"
 output 
-Enter the number of a table : 5
-Enter the end number : 10
-5 x 1 =  5
-5 x 2 =  10
-5 x 3 =  15
-5 x 4 =  20
-5 x 5 =  25
-5 x 6 =  30
-5 x 7 =  35
-5 x 8 =  40
-5 x 9 =  45
-5 x 10 =  50
+Enter the number of a table : 4
+Enter the end number : 5
+1 x 1 =  1
+1 x 2 =  2
+1 x 3 =  3
+1 x 4 =  4
+1 x 5 =  5
+2 x 1 =  2
+2 x 2 =  4
+2 x 3 =  6
+2 x 4 =  8
+2 x 5 =  10
+3 x 1 =  3
+3 x 2 =  6
+3 x 3 =  9
+3 x 4 =  12
+3 x 5 =  15
+4 x 1 =  4
+4 x 2 =  8
+4 x 3 =  12
+4 x 4 =  16
+4 x 5 =  20
 
 """
+
